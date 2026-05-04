a0420a01fac2767580ea86157a1bf85fbfaad644
Chaynika0306 <vyas.chaynika17@gmail.com>
2026-05-04 11:45:13 +0530

    updated the code

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",        // ✅ SMTP server address
   port: 465,                     // ✅ TLS port
-  secure: false,                 // ✅ false for port 587
+  secure: true,                 // ✅ false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
